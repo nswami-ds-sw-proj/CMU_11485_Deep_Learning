b86d2c383684bea482cd2162c6829265f7e0d417
nswami-ds-sw-proj <40180416+nswami-ds-sw-proj@users.noreply.github.com>
2021-01-20 14:43:15 -0500

    Update loss.py

--- HW2/P1/mytorch/nn/loss.py
+++ HW2/P1/mytorch/nn/loss.py
@@ -38,5 +38,4 @@ class CrossEntropyLoss(Loss):
             Tensor: loss, stored as a float in a tensor 
         """
         # Simply calls nn.functional.cross_entropy
-        # If you implement your own Function subclass you may need to modify this
         return F.cross_entropy(predicted, target)
